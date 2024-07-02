22322c5aaa321b7e66c9bab7dbc100b692c6f006
Rimon <mdraisul.i.rimon@gmail.com>
2024-07-02 23:29:26 +0600

    submit a code

---
+++ 1989A-CatchTheCoin.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,x1,y1;
+    cin>>n;
+
+    while(n--)
+    {
+        cin>>x1>>y1;
+        if(y1>=-1)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+
+    return 0;
+}
