cb7ec5236698355613f00f5712fd70cad72e545c
Rimon <mdraisul.i.rimon@gmail.com>
2024-07-28 18:05:25 +0600

    add a code

---
+++ 1985B-2-MaximumMultipleSum.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        
+        cout<<(n==3 ? 3 :2) <<endl;//if n is equal to 3 then ans is 3,else ans is 2/logic is in exercise book
+    }
+    return 0;
+}
